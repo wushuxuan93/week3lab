a4da146eb4f0700b67926b5290992006d3d505e8
Shuxuan Wu <wushuxuan93@gmail.com>
2022-09-22 10:10:34 -0400

    first commit

---
+++ .history/routes/index_20220922090505.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Us' });
+});
+router.get('/Products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+module.exports = router;
